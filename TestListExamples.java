f841db500a6dc6df0211573663b9df7ba1c6ea69
Awakenight <jkphung@ucsd.edu>
2022-11-18 05:20:56 -0800

    a

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,28 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.List;
+import java.util.Arrays;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test(timeout=100)
+  public void testFilter() {
+    List<String> expected = Arrays.asList(new String[] {"a", "ab"}) 
+    List<String> toBeFilter = Arrays.asList(new String[] {"a", "bc", "ab"})
+
+    List<String> filtered = ListExamples.filter(toBeFilter, (String s) -> s.contains("a"));
+
+    assertEquals(expected, filtered);
+  }
+
+  @Test(timeout=100)
+  public void testMerge() {
+    List<String> expected = Arrays.asList(new String[] {"a", "a", "b", "b", "c", "c"})
+    List<String> toBeMerged = Arrays.asList(new String[] {"a", "b", "c"}) 
+
+    List<String> merged = ListExamples.merge(toBeMerged, toBeMerged);
+
+    assertEquals(expected, filtered);
+  }
+
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,48 @@
 # Create your grading script here
 
-set -e
+# set -e
+
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
+TotalGrade=2
+file="ListExamples.java"
 
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission
 
+if [[ -f $file ]] && [[ -e $file ]]
+then
+    echo "File found!"
+else
+    echo "File not found!"
+    exit 1
+fi
+
+cp TestListExamples.java student-submission/
+javac -cp $CPATH *.java
+
+if [[ $? -eq 0 ]]
+then
+    echo "Compile succeeded"
+else
+    echo "Program unable to compile"
+    exit 2
+fi
+
+java -cp $CPATH  org.junit.runner.JUnitCore TestListExamples > errors.txt
+
+if [[ $(grep -c "testFilter(TestListExamples)" errors.txt) -eq 1 ]]
+then
+     ((TotalGrade-=1))
+    let "TotalGrade-=1"
+    echo "[FAILED 0/1] testFilter"
+fi
+
+if [[ $(grep -c "testMerge(TestListExamples)" errors.txt) -eq 1 ]]
+then
+    ((TotalGrade-=1))
+    let "TotalGrade-=1"
+    echo "[FAILED 0/1] testMerge"
+fi
+
+echo Total Grade: $totalPoints/2
